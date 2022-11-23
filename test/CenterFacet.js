5b4180eb4a187b310101245406b4fa14e902970b
ethan-sibs <102136771+ethan-sibs@users.noreply.github.com>
2022-11-23 23:46:34 +1100

    Working mint function w/ signatures

--- test/CenterFacet.js
+++ test/CenterFacet.js
@@ -8,29 +8,44 @@ beforeEach(async () => {
     CenterFacet = await ethers.getContractAt('CenterFacet', diamondAddress)
     await CenterFacet.setAuthorisedSigner(deployer.address)
 
-    getSignature = async (addr) => {
-        return await deployer.signMessage(ethers.utils.arrayify(addr))
+    getSignature = async (tokenId, addr) => {
+        let payload = ethers.utils.hexConcat(
+            [
+                addr,
+                ethers.utils.hexZeroPad(ethers.utils.hexlify(tokenId), 32)
+            ]
+        )
+        return await deployer.signMessage(ethers.utils.arrayify(payload))
     }
 })
 
 describe("CenterFacet", () => {
-    it("Mint function accepts valid signature", async () => {
-        
+    it("Mint function accepts valid signatures", async () => {
+        let tokenId = 69
+        await CenterFacet.mint(tokenId, await getSignature(tokenId, deployer.address))
+
+        tokenId = 420
+        await CenterFacet.connect(addr1).mint(tokenId, await getSignature(tokenId, addr1.address))
     })
 
-    it("Mint function rejects invalid signature", async () => {
+    it("Mint function rejects invalid signatures", async () => {
+        let tokenId = 69
 
+        // incorrect tokenId
+        let invalidSig = await getSignature(tokenId - 1, deployer.address)
+        await expect(CenterFacet.mint(tokenId, invalidSig))
+        .to.be.revertedWith("CenterFacet: invalid signature")
+
+        // incorrect address
+        invalidSig = await getSignature(tokenId, addr1.address)
+        await expect(CenterFacet.mint(tokenId, invalidSig))
+        .to.be.revertedWith("CenterFacet: invalid signature")
     })
 
     it("Minted tokens have correct IDs", async () => {
-        expect(await CenterFacet.exists(99)).to.equal(false)
+        let tokenId = 69
+        await CenterFacet.mint(tokenId, await getSignature(tokenId, deployer.address))
 
-        let sig = await getSignature(deployer.address)
-
-        // let hash = ethers.utils.hashMessage(ethers.utils.arrayify(deployer.address))
-        // console.log(deployer.address, hash, sig)
-
-        await CenterFacet.mint(99, sig)
-        expect(await CenterFacet.exists(99)).to.equal(true)
+        expect(await CenterFacet.exists(tokenId)).to.equal(true)
     })
 })
